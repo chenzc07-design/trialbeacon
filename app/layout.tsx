7f12a9efbe0f3b12a69b68baf1bc06aa05e59183
chenzc07-design <chenzc07@gmail.com>
2026-08-01 11:16:34 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: 'device-width',
   initialScale: 1,
-  themeColor: '#1e3350',
+  themeColor: '#0F172A',
 };
 
 export default async function RootLayout({
